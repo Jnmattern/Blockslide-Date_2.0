4c6b927aa712f94a2b58b3f0e0370b5786258b96
Ron Ravid <ron64@post.com>
2014-06-18 20:27:26 +0900

    Added round corners to make prettier digits. Fixed timer maintenance. Updated version and configuration files. Added minor destroy. ToDo: 1. Continue the table for all other symbols and letters. Some like the letter M, can look much better(middle square with GCornersBottom). Editor may be needed. Binary representation may be easier than doing || on several GCorner values. 2. Check the configuration update.

--- src/Blockslide-Date_2.0.h
+++ src/Blockslide-Date_2.0.h
@@ -71,3 +71,20 @@ int8_t digits[][13][2] = {
 	{ {7,0}, {7,0}, {7,0}, {6,1}, {6,1}, {5,2}, {5,2}, {5,2}, {4,3}, {4,3}, {3,4}, {3,4}, {3,4} },				// 47 // SpaceRight
 	{ {0,5}, {1,5}, {2,5}, {0,6}, {2,6}, {0,7}, {1,7}, {2,7}, {0,8}, {2,8}, {0,9}, {1,9}, {2,9} }				// 48 // Space down
 };
+
+uint8_t digit_corner[][13] = {
+	{GCornerTopLeft, GCornerNone, GCornerTopRight, GCornerNone, GCornerNone, GCornerNone, GCornerNone, GCornerNone, GCornerNone, GCornerNone, GCornerBottomLeft, GCornerNone, GCornerBottomRight}, 	//0
+	{GCornersTop, GCornersTop, GCornersTop, GCornersLeft, GCornerNone, GCornerNone, GCornerNone, GCornerNone,  GCornerNone, GCornerNone, GCornersLeft, GCornerNone, GCornersRight }, 				//1
+	{GCornersLeft, GCornerTopRight, GCornerTopRight, GCornerTopRight, GCornerTopRight, GCornerTopLeft, GCornerNone, GCornerBottomRight, GCornerNone, GCornerNone,   GCornerBottomLeft, GCornerNone, GCornersRight}, //2
+	{GCornersLeft, GCornerNone, GCornerTopRight,  GCornerNone, GCornerNone,   GCornersLeft, GCornerNone, GCornerNone, GCornerNone, GCornerNone, GCornersLeft, GCornerNone, GCornerBottomRight}, 	//3
+	{GCornersTop, GCornersTop, GCornersTop, GCornerBottomLeft, GCornerNone,  GCornerBottomLeft, GCornerBottomLeft, GCornerNone, GCornerNone, GCornerNone,   GCornersBottom, GCornersBottom, GCornersBottom}, //4
+	{GCornerTopLeft, GCornerNone, GCornersRight,  GCornerNone, GCornerNone,    GCornerBottomLeft, GCornerNone, GCornerTopRight, GCornerBottomRight, GCornerBottomRight, GCornersLeft, GCornerBottomRight, GCornerBottomRight}, //5
+	{GCornerTopLeft, GCornerNone, GCornersRight,  GCornerNone, GCornerNone,		GCornerNone, GCornerNone, GCornerTopRight,  GCornerNone, GCornerNone,   GCornerBottomLeft, GCornerNone, GCornerBottomRight}, //6
+	{GCornersLeft, GCornerNone, GCornerTopRight,  GCornerBottomRight, GCornerBottomRight,    GCornerTopLeft, GCornerTopLeft, GCornerTopLeft,  GCornerNone, GCornerNone,   GCornersBottom, GCornersBottom, GCornersBottom}, //7
+	{GCornerTopLeft, GCornerNone, GCornerTopRight,  GCornerNone, GCornerNone,   GCornerNone, GCornerNone, GCornerNone,GCornerNone, GCornerNone,   GCornerBottomLeft, GCornerNone, GCornerBottomRight}, //8
+	{GCornerTopLeft, GCornerNone, GCornerTopRight,  GCornerNone, GCornerNone,   GCornerBottomLeft, GCornerNone, GCornerNone, GCornerNone, GCornerNone,   GCornersLeft, GCornerNone, GCornerBottomRight}, //9
+	{GCornersAll,GCornersAll,GCornersAll,GCornersAll,GCornersAll,GCornersAll,GCornersAll,GCornersAll,GCornersAll,GCornersAll,GCornersAll,GCornersAll,GCornersAll}, 									//:
+	{GCornersAll,GCornersAll,GCornersAll,GCornersAll,GCornersAll,GCornersAll,GCornerBottomRight, GCornersAll, GCornerBottomRight, GCornerBottomRight, GCornerBottomRight||GCornerTopLeft, GCornerBottomRight||GCornerTopLeft, GCornerBottomRight||GCornerTopLeft}, //;
+	{GCornerNone, GCornerNone, GCornerNone, GCornerNone, GCornerNone, GCornerNone, GCornerNone, GCornerNone, GCornerNone, GCornerNone, GCornerNone, GCornerNone, GCornerNone } 	//no corners
+};
+#define LAST_C 12
